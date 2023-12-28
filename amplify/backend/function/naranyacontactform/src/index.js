4646e577ba096a9edc9e488b6e4f4e114d997236
Arturo Vélez <velez.arturo@gmail.com>
2023-12-28 11:54:59 -0600

    update para soportar versión de node moderna

--- amplify/backend/function/naranyacontactform/src/index.js
+++ amplify/backend/function/naranyacontactform/src/index.js
@@ -1,5 +1,6 @@
-const aws = require('aws-sdk')
-const ses = new aws.SES()
+const { SESv2Client, SendEmailCommand } = require("@aws-sdk/client-sesv2"); // CommonJS import
+
+const client = new SESv2Client({ region: "us-east-2" });
 
 exports.handler = async (event) => {
   for (const streamedItem of event.Records) {
@@ -8,20 +9,45 @@ exports.handler = async (event) => {
       const candidateName = streamedItem.dynamodb.NewImage.name.S
       const candidateEmail = streamedItem.dynamodb.NewImage.email.S
 
-      await ses
-          .sendEmail({
-            Destination: {
-              ToAddresses: [process.env.SES_EMAIL],
+    
+      
+      const input = { // SendEmailRequest
+        FromEmailAddress: process.env.SES_EMAIL,
+        // FromEmailAddressIdentityArn: "STRING_VALUE",
+        Destination: { // Destination
+          ToAddresses: [ // EmailAddressList
+            process.env.SES_EMAIL,
+          ],
+
+        },
+        ReplyToAddresses: [
+          process.env.SES_EMAIL,
+        ],
+        Content: { // EmailContent
+          Simple: { // Message
+            Subject: { // Content
+              Data: "Contacto Registrado", // required
+              // Charset: "STRING_VALUE",
             },
-            Source: process.env.SES_EMAIL,
-            Message: {
-              Subject: { Data: 'Candidate Submission' },
-              Body: {
-                Text: { Data: `My name is ${candidateName}. You can reach me at ${candidateEmail}` },
+            Body: { // Body
+              Text: {
+                Data: `El contacto ${candidateEmail} ha sido registrado `, // required
+                // Charset: "STRING_VALUE",
               },
-            },
-          })
-          .promise()
+              Html: {
+                Data: `El contacto <h1> ${candidateEmail} </h1> ha sido registrado `, // required
+                // Charset: "STRING_VALUE",
+              }
+            }
+          }
+        }
+      };
+
+      console.log({ input })
+      const command = new SendEmailCommand(input);
+      const response = await client.send(command);
+      console.log({ response })
+
     }
   }
   return { status: 'done' }
