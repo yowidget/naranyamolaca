ab0ad4c3e3859927d426237814388dea5d6297a0
Arturo Vélez <velez.arturo@gmail.com>
2024-02-16 22:32:39 -0600

    setup molaca

--- src/layouts/function-components/Contact.jsx
+++ src/layouts/function-components/Contact.jsx
@@ -6,7 +6,7 @@ import { createLead } from '../../graphql/mutations'
 import { useState, useCallback } from "react";
 
 import { GoogleReCaptchaProvider, useGoogleReCaptcha } from 'react-google-recaptcha-v3';
-const GRECAPTCHA_SITEKEY = "6Ld3wGMpAAAAAGtn7TNxbTjhNoPTYuHPT0Kd2cGd";
+const GRECAPTCHA_SITEKEY = "6LeraT8pAAAAAEWxiV6mEY6FHRE6n5tqOIo61hrw";
 
 const client = generateClient()
 
